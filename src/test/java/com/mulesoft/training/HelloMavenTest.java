22a4faa3cc14b916c677e4e0f4f7e1cb5e73ee99
vasanth-murugesan <vasanthakumar.murugesan@gmail.com>
2018-02-20 17:40:16 -0500

    port changed1

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -9,7 +9,7 @@ public class HelloMavenTest extends FunctionalTestCase {
 
     @Test
     public void mavenFlowReturnsHelloMaven() throws Exception {
-        runFlowAndExpect("mavenFlow", "Hello Maven");
+        runFlowAndExpect("mavenFlow", "Hello World");
     }
     
     @Override
